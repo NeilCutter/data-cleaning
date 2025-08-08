edc101b4e3ecf90acb3ed00aa7744127687354d8
neilcutter <neilcanas08@gmail.com>
2025-08-08 15:04:41 +0800

    update filename_converter

--- webapp/filename_converter.py
+++ webapp/filename_converter.py
@@ -1,12 +1,12 @@
 from webapp import pandas as pd
 from webapp import os
 from webapp import glob
-import datetime
+from dotenv import load_dotenv
 
+load_dotenv()
 
-MONTH = "07"
-YEAR = "2025"
-
+MONTH:str = os.getenv("MONTH")
+YEAR:str = os.getenv("YEAR")
 
 def filenames_to_dates(PATH):
     files = list(filter(os.path.isfile, glob.glob(PATH + "\\*")))
@@ -14,7 +14,9 @@ def filenames_to_dates(PATH):
     
     for file in files:
         DAY = str(files.index(file) + 1)
-        if file.split("\\")[5] != "Southstar": 
-            os.rename(file, rf"{PATH}\{MONTH}.{DAY.zfill(2)}.{YEAR}.xlsx")
+        if file.split("\\")[5] != "Southstar":
+            # os.rename(file, rf"{PATH}\{MONTH}.{DAY.zfill(2)}.{YEAR}.xlsx")
+            os.rename(file, rf"{PATH}\{MONTH}.{DAY}.{YEAR}.xlsx")
         else:
-            os.rename(file, rf"{PATH}\{MONTH}.{DAY.zfill(2)}.{YEAR}.xls")
+            # os.rename(file, rf"{PATH}\{MONTH}.{DAY.zfill(2)}.{YEAR}.xls")
+            os.rename(file, rf"{PATH}\{MONTH}.{DAY}.{YEAR}.xls")
